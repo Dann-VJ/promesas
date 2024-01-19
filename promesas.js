cbaa5c4637701363686e513c48b2c8461e6b8003
Dann-VJ <danielvazquezjoaquin236@gmail.com>
2024-01-19 17:25:08 -0600

    Promise.all

--- promesas.js
+++ promesas.js
@@ -17,6 +17,7 @@ const sumarRapido = (numero) => {
 
 // Promise.all ejecuta todas las promesas y devuelve un arreglo con los resultados
 // Si falla una, fallan todas
+// Ejecuta todas las promesas en paralelo
 Promise.all([sumarLento(5), sumarRapido(10)])
     .then(respuestas => {
         console.log(respuestas);
