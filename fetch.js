01510ed03accd24d6e916b0500641046416dfceb
Dann-VJ <danielvazquezjoaquin236@gmail.com>
2024-01-22 17:44:46 -0600

    Fetch post

--- fetch.js
+++ fetch.js
@@ -1,12 +1,17 @@
-// Peticion get
+// Peticion post
 
-fetch('https://platzi.com')
-    .then(res => res.text())
-    .then(html => {
-        document.open()
-        document.write(html)
-        document.close()
-    })
+let user = {
+    name: 'John',
+    surname: 'Smith'
+};
 
-
-    
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify(user),
+    headers: {
+        'Content-Type': 'application/json'
+    }gi
+})
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.log(error));
