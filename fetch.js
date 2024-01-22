28b4469952912cf4205206b997a48e1d22315447
Dann-VJ <danielvazquezjoaquin236@gmail.com>
2024-01-22 17:29:09 -0600

    Petición de productos

--- fetch.js
+++ fetch.js
@@ -1,5 +1,16 @@
 // Peticion get
 
-fetch('https://jsonplaceholder.typicode.com/users')
+fetch('https://fakestoreapi.com/products')
     .then(response => response.json())
     .then(json => console.log(json))
+
+fetch('https://fakestoreapi.com/products/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+// Traer el producto con id 1 y mostrar solo el precio
+fetch('https://fakestoreapi.com/products/1')
+    .then(response => response.json())
+    .then(json => console.log(json.price))
+
+    git 
