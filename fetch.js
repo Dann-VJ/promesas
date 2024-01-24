56fb06369ed5805a6a9586262783292b71ee7d7a
Dann-VJ <danielvazquezjoaquin236@gmail.com>
2024-01-24 17:22:23 -0600

    Fetch con clone

--- fetch.js
+++ fetch.js
@@ -1,26 +1,15 @@
-// Fetch erchives from the server
-
-/* En la carpeta img realiza una solicitud de fetch a esta imagen.png 
-y muestra la imagen.png en el navegador */
-
-
-fetch('http://127.0.0.1:5501/img/business-people.png')
-    .then(resp => resp.type === 'basic' ? resp.blob() : resp.text())
-    .then(data => {
-        if (typeof data === 'string') {
-            console.log('Es un string')
-            document.open()
-            document.write(data)
-            document.close()
-        } else {
-            console.log('Es un blob')
-            const img = document.createElement('img')
-            console.log(img)
-            img.src = URL.createObjectURL(data)
-            document.body.append(img)
-        }
+// Solicitud para poder clonar la respuesta y poder usarla varias veces
+fetch('https://pokeapi.co/api/v2/pokemon/2')
+    .then(res => {
+        res.clone().json().then(data => {
+            console.log(data);
+        })
+        res.clone().json().then(data => {
+            console.log(data);
+        })
+        res.json().then(data => {
+            console.log(data);
+        })
     })
 
-
-
-
+    
