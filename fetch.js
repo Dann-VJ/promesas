264f861faf17779333cd0bc4af98a70f0a8e307b
Dann-VJ <danielvazquezjoaquin236@gmail.com>
2024-01-24 17:26:30 -0600

    Fetch con error

--- fetch.js
+++ fetch.js
@@ -1,15 +1,10 @@
-// Solicitud para poder clonar la respuesta y poder usarla varias veces
-fetch('https://pokeapi.co/api/v2/pokemon/2')
-    .then(res => {
-        res.clone().json().then(data => {
-            console.log(data);
-        })
-        res.clone().json().then(data => {
-            console.log(data);
-        })
-        res.json().then(data => {
-            console.log(data);
-        })
+//  Manejor de error
+fetch('https://poeapi.co/api/v2/pokemon/2')
+    .then(response => {
+        response.json().then(console.log)
+    })
+    .catch(error => {
+        console.log('Error en la petición')
+        console.log(error)
     })
 
-    
